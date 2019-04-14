2c4ab80d51c4ac855c4fee9fdb4017d58fcc07d4
Mk <mark.staryshko666@gmail.com>
2019-04-14 22:04:25 +0300

    work on js part2

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var Game = function(options) {
+  this.bubbles = options.bubbles;
+  this.bubblesArr = [];
+  this.score = 0;
+  this.duration = options.duration;
+  this.start = function() {
+    
+    setTimeout(function() {
+      this.finish();
+    }, this.duration);
+    
+    for (i = 0; i < this.bubbles; i++) {
+      // Wait random amount of time between creating bubbles
+      setTimeout(function() {
+        var newBubble = new Bubble();
+        newBubble.blow();
+      }, Math.floor(Math.random() * this.duration - 800) + 0);
+    };
+   
