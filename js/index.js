b77a2fe9eb943ed1e8a4ad1a89e2b490fe8092b6
Mark Staryshko <36264025+Mark071198@users.noreply.github.com>
2019-06-06 19:06:21 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,114 +2,133 @@
 *Переменной Game присваивается значение функции в качестве параметров для которой передаются количество 
 *пузырей (bubbles) и время подачи пузырей (duration)
 *
-*@bubbles - количество пузырей
-*@bubblesArr - массив для генерации пузырей
-*@score - счет
-*@duration - длительность
-**/
-var Game = function(options) {
-    this.bubbles = options.bubbles;
-    this.bubblesArr = [];
-    this.score = 0;
-    this.duration = options.duration;
-    /*Дейсвтия производимые в начале игры*/  
-    this.start = function() {
-    
-        setTimeout(function() {
-            this.finish();
-        }, this.duration);
-    
-        for (i = 0; i < this.bubbles; i++) {
-            /*Ожидает случайного количетсво времени между генерацией пузырей*/
-            setTimeout(function() {
-                var newBubble = new Bubble();
-                newBubble.blow();
-            }, Math.floor(Math.random() * this.duration - 800) + 0);
-        };
-        /*Выставляется обрытный отсет на 16 секунд*/ 
-        var seconds_left = this.duration / 1000;
-        var interval = setInterval(function() {
-            $('#timer_div').html(--seconds_left);
+*@param {integer} bubbles - количество пузырей
+*@param {integer} bubblesArr - массив для генерации пузырей
+*@param {integer} score - счет
+*@param {integer} duration - длительность
+*/
+    var Game = function(options) {
+        this.bubbles = options.bubbles;
+        this.bubblesArr = [];
+        this.score = 0;
+        this.duration = options.duration;
 
-                if (seconds_left <= 0) {
-                    $('#timer_div').html("Time is over"); 
-                    $('#create').html("Play again?");
-                    $('#create').show();
-                    clearInterval(interval);
-                }
-        }, 1000);
-   };
+        /**
+        *Дейсвтия производимые в начале игры
+        *
+        *@method start 
+        *@param {object} newBubble - объявления класса Bubble
+        *@param {integer} second_left - длительность
+        *@param {object} interval - интервал между генерацией пузырьков
+        */  
+        this.start = function() {
+    
+            setTimeout(function() {
+                this.finish();
+            }, this.duration);
+    
+            for (i = 0; i < this.bubbles; i++) {
+                /*Ожидает случайного количетсво времени между генерацией пузырей*/
+                setTimeout(function() {
+                    var newBubble = new Bubble();
+                    newBubble.blow();
+                }, Math.floor(Math.random() * this.duration - 800) + 0);
+            };
+            /*Выставляется обрытный отсчет на 16 секунд*/ 
+            var seconds_left = this.duration / 1000;
+            var interval = setInterval(function() {
+                $('#timer_div').html(--seconds_left);
+
+                    if (seconds_left <= 0) {
+                        $('#timer_div').html("Time is over"); 
+                        $('#create').html("Play again?");
+                        $('#create').show();
+                        clearInterval(interval);
+                    }
+            }, 1000);
+       };
 
  
    
-};
+    };
 
 /* Старт игры и начало генерации пузырей*/
-$('#create').click(function() {
-    game.score=0;
-    $('#score').html('Score : ' + game.score);
-    game.start();
-    $('#create').hide();
+    $('#create').click(function() {
+        game.score=0;
+        $('#score').html('Score : ' + game.score);
+        game.start();
+        $('#create').hide();
 })
 
 /**
 *
 *Функция Bubble создает пузыри, выставляет их размер и позицию
 *
-*@animete - метод анимации
-*@blow - запускает поток пузырей, задает их размер и направление
-*@pop - считает количество лопнутых пузырей
-**/
-function Bubble(left, top) {
-  this.id = game.bubblesArr.length;
+*/
+    function Bubble(left, top) {
+      this.id = game.bubblesArr.length;
 
-  //Animate this bubble
-  this.animate = function() {
-    $("#bubble-" + this.id).animate({
-      top: -100,
-    }, Math.floor(Math.random() * 2100) + 2000)
-  }
+      /**
+      *Анимация
+      *
+      *@method animate
+      */
+      this.animate = function() {
+        $("#bubble-" + this.id).animate({
+          top: -100,
+        }, Math.floor(Math.random() * 2100) + 2000)
+      }
+      /**
+      *Дейсвтивя с пузырями
+      *
+      *@method blow
+      *@param {integer} num - количество лопнутых пузырьков
+      *@param {integer} pageWidth - ширина 
+      *@param {integer} size - размер
+      */
+      this.blow = function() {
 
-  this.blow = function() {
-
-    /*Добавляет пузыри на страницу*/
-    $('#container').append('<div class="bubble" id="bubble-' + this.id + '"></div>');
+        /*Добавляет пузыри на страницу*/
+        $('#container').append('<div class="bubble" id="bubble-' + this.id + '"></div>');
     
-    game.bubblesArr.push(this);
+        game.bubblesArr.push(this);
+        /*Считает лопнутые пузыри*/  
+        $('#bubble-' + this.id).click(function() {
+          var num = this.id.replace('bubble-', '');
+          game.bubblesArr[num].pop();
+        });
 
-    $('#bubble-' + this.id).click(function() {
-      var num = this.id.replace('bubble-', '');
-      game.bubblesArr[num].pop();
-    });
+        /* Рандомизация размеров и позиции*/
+        var pageWidth = $(document).width();
+        $('#bubble-' + this.id).css("left", Math.floor(Math.random() * pageWidth / 100 * 35) + 20);
+        var size = Math.floor(Math.random() * 60) + 50 + "px";
+        $('#bubble-' + this.id).css("width", size);
+        $('#bubble-' + this.id).css("height", size);
 
-   /* Рандомизация размеров и позиции*/
-    var pageWidth = $(document).width();
-    $('#bubble-' + this.id).css("left", Math.floor(Math.random() * pageWidth / 100 * 35) + 20);
-    var size = Math.floor(Math.random() * 60) + 50 + "px";
-    $('#bubble-' + this.id).css("width", size);
-    $('#bubble-' + this.id).css("height", size);
+        this.animate();
 
-    this.animate();
+      }
+    /**
+    *
+    *Ведет счет лопнутых пузырей
+    *
+    *@method pop
+    */  
+    this.pop = function() {
+        $('#bubble-' + this.id).hide();
+        game.score++;
+        $('#score').html('Score : ' + game.score);
+    }
 
   }
-
-  this.pop = function() {
-    $('#bubble-' + this.id).hide();
-    game.score++;
-    $('#score').html('Score : ' + game.score);
-  }
-
-}
 
 /**
 *
 *Переменной game присваевается значение переменной-функции Game с параметрами 
-*bubbles - количество пузырей 
-*duration - продолжительность
 *
-**/
-var game = new Game({
-  bubbles: 20,
-  duration: 16000
-});
+*/
+    var game = new Game({
+      bubbles: 20,
+      duration: 16000
+    });
 
